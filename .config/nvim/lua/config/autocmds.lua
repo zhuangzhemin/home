5af9279782bb134d171f603056c7489c0810fbd7
zhuangzhemin <zhuangzhemin@qq.com>
2024-04-24 18:07:41 +0800

    update git config and nvim config

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -22,19 +22,24 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
   pattern = { "*.bean", "*.beancount"},
   callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
     vim.opt_local.iskeyword:append(":")
     vim.opt_local.iskeyword:append("-")
     vim.opt_local.filetype = "beancount"
+    -- vim.cmd("setlocal iskeyword+=:")
+    -- vim.cmd("setlocal iskeyword+=-")
   end,
 })
 
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-  pattern = {"*"},
-  callback = function(event)
-    local buf = event.buf
-    if vim.bo[buf].filetype == "lazyterm" then
-      return
-    end
-    vim.cmd("lcd %:p:h")
-  end
-})
+-- vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+--   pattern = {"*"},
+--   callback = function(event)
+--     local buf = event.buf
+--     if vim.bo[buf].filetype == "lazyterm" then
+--       return
+--     end
+--     vim.cmd("lcd %:p:h")
+--   end
+-- })
--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -12,10 +12,10 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.editor.trouble-v3" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.python" },
+    -- { import = "lazyvim.plugins.extras.dap.core" },
+    -- { import = "lazyvim.plugins.extras.editor.trouble-v3" },
+    -- { import = "lazyvim.plugins.extras.lang.markdown" },
+    -- { import = "lazyvim.plugins.extras.lang.python" },
     -- import/override with your plugins
     { import = "plugins" },
   },
